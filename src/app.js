8e6255dfbf866d1b38158edda98db385403aec35
Derek Nunes <dereknunes28@gmail.com>
2022-06-22 00:28:40 -0500

    update unsplash image selection, inital modal styling, search button to update backround image

--- src/app.js
+++ src/app.js
@@ -6,40 +6,52 @@
 // default background and weather data
 const initialPage = () => {
     let defaultValue = 'Kansas+City';
-    loadgif(defaultValue);
+    unsplashBg(defaultValue);
 }
 
 // update background
-const updateBg = () => {
+const searchCity = () => {
+    const seachButton = document.querySelector('#searchButton');
+    seachButton.addEventListener('click', function(e) {
+        updateBg();
+    });
 
-
+    document.addEventListener('keydown', (e) => {
+        if (e.key ==='Enter') {
+          updateBg();
+        }
+    });
 }
 
 export { initialPage };
+export { searchCity };
 
 
 // background image
-
-function loadgif(value) {
+// load unsplash image
+function unsplashBg(value) {
     const img = document.querySelector('#bgImage');
     const client_id = "v1qiZp2SUUh-SN7uY-Kg5s8qkPtUgcj3vHM_dSGuwTI"
-    fetch(`https://api.unsplash.com/search/photos/?query=${value}$page=1&per_page=1&client_id=${client_id}`)
-        .then(function(response) {
-            console.log(response);    
+    fetch(`https://api.unsplash.com/search/photos?query=${value}&client_id=${client_id}`)
+        .then(function(response) {    
             return response.json();
         })
         .then(function(response) {
-            // const imageSrc = response.results.urls.regular;
-            // console.log(imageSrc);
-            // img.src = response.results.urls.regular;
-            // console.log(img.src);
             console.log(response);
-            response.results.forEach(photo => {
-                img.src = photo.urls.regular;
-            });
+            let selectedPhoto = Math.floor(Math.random() * 10);
+            img.src = response.results[selectedPhoto].urls.regular;
+            // response.results.forEach(photo => {
+            //     img.src = photo.urls.regular;
+            // });
         })
         .catch(function() {
             // add default image or message to DOM if it fails
             console.log('Cannot retrieve data');
         });
+}
+
+function updateBg() {
+    let searchValue = document.querySelector('.weatherSearch').value;
+    searchValue = searchValue.split(' ').join('+');
+    unsplashBg(searchValue);
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import { initialPage } from './app';
+import { searchCity } from './app';
 
 initialPage();
+searchCity();
 
