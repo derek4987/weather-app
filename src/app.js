d24d20cabf15b8216dba16885ef49495038e0fa6
Derek Nunes <dereknunes28@gmail.com>
2022-06-23 00:48:35 -0500

    fetch weather api data based on input

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 // single page weather app js 
 
+let didExecute =true;
+let weatherConstructor;
 
 // functions to import
 
@@ -13,14 +15,20 @@ const initialPage = () => {
 const searchCity = () => {
     const seachButton = document.querySelector('#searchButton');
     seachButton.addEventListener('click', function(e) {
-        updateBg();
+        openWeather();
+        if (didExecute === true){
+            updateBg();
+        } else return;
     });
 
     document.addEventListener('keydown', (e) => {
-        if (e.key ==='Enter') {
-          updateBg();
+        if (e.key ==='Enter' && document.querySelector('.weatherSearch') === document.activeElement) {
+            openWeather();
+            if (didExecute === true) {
+                updateBg();
+            } else return;
         }
-    });
+    });    
 }
 
 export { initialPage };
@@ -54,4 +62,49 @@ function updateBg() {
     let searchValue = document.querySelector('.weatherSearch').value;
     searchValue = searchValue.split(' ').join('+');
     unsplashBg(searchValue);
+}
+
+// weather data
+// fetch data with open weather api
+async function openWeather() {
+    const cityInput = document.querySelector('.weatherSearch').value;
+    const api_key = '3e5943bf2df310039c464f9b62bbd804';
+
+    try {
+        didExecute = true;
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${api_key}`);
+        const responseData = await response.json();
+        console.log(responseData);
+
+        const city = await responseData.name;
+        const temp = await responseData.main.temp;
+        const tempHigh = await responseData.main.temp_max;
+        const tempLow = await responseData.main.temp_min;
+        const humidity = await responseData.main.humidity;
+        const feelsLike = await responseData.main.feels_like;
+        const description = await responseData.weather[0].main;
+        const windSpeed = await responseData.wind.speed;
+
+        const weatherData = new WeatherData(city,temp,tempHigh,tempLow,humidity,feelsLike,description,windSpeed);
+        weatherConstructor = weatherData;
+        console.log(weatherData);
+    } catch(err) {
+        didExecute = false;
+        err = 'City not found';
+        alert(err);        
+    }
+    
+    
+}
+
+// weather data constructor
+function WeatherData(city, temp, tempHigh, tempLow, humidity, feelsLike, description, windSpeed) {
+    this.city = city;
+    this.temp = temp;
+    this.tempHigh = tempHigh;
+    this.tempLow = tempLow;
+    this.humidity = humidity;
+    this.feelsLike = feelsLike;
+    this.description = description;
+    this.windSpeed = windSpeed;
 }
