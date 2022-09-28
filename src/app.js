4b8627ad61125dcb04b1ed269c3892d3a17cc561
Derek Nunes <dereknunes28@gmail.com>
2022-09-27 21:29:25 -0700

    weather data api to fetch from https vs http

--- src/app.js
+++ src/app.js
@@ -89,7 +89,7 @@ async function openWeather() {
 
     try {
         didExecute = true;
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${api_key}`);
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${api_key}`);
         const responseData = await response.json();
         console.log(responseData);
 
